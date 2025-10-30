286afc4ccb69c8b6c6aecac4e6bb09286de38f6d
HeberC2003 <00054621@uca.edu.sv>
2025-10-30 03:23:45 -0600

    Arreglando rutas

--- nextplace-frontend/src/routes/AppRouter.jsx
+++ nextplace-frontend/src/routes/AppRouter.jsx
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
 // src/routes/AppRouter.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/user/Home/Home";
-
-export default function AppRouter() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
-}
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "../pages/user/Login/login";
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
       </Routes>
     </Router>
   );
 }
->>>>>>> feature/Login
