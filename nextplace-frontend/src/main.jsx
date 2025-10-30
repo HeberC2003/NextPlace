5ff4f0e8d497801507df671a27b0fba58e8dbf88
HeberC2003 <00054621@uca.edu.sv>
2025-10-30 11:15:01 -0600

    solo verificando

--- nextplace-frontend/src/main.jsx
+++ nextplace-frontend/src/main.jsx
@@ -1,20 +1,11 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import App from "./App";
 import "./index.css"; // Tailwind + CSS global
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-=======
-import AppRouter from "./routes/AppRouter.jsx";
-import "./index.css";   // Tailwind aquí
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppRouter />
->>>>>>> feature/Heber-eventlist
   </React.StrictMode>
 );
